5b0700adf38713605f2d22eb86bdc3eb42a9812a
Krinesh <pkrinesh95@gmail.com>
2023-07-19 12:43:21 +0530

    feat: how we can use second script to define type and import from another component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,27 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// TODO: vue-router integration
+import { ref } from 'vue'
+import TheName, { type TNameProps } from './components/TheName.vue'
+
+const data = ref<TNameProps>({ name: 'Krinesh', age: 27 })
+</script>
 
 <template>
-	<h1 class="heading">Personal Vue Playground</h1>
+	<main>
+		<h1 class="heading">Personal Vue Playground</h1>
+		<p>{{ data.name }} {{ data.age }}</p>
+		<TheName :name="'Krupa'" :age="26" />
+	</main>
 </template>
 
 <style scoped>
 .heading {
 	margin-top: 2rem;
 	text-align: center;
+}
+
+main {
+	display: grid;
+	place-items: center;
 }
 </style>
---
+++ src/components/TheName.vue
@@ -0,0 +1,25 @@
+<script lang="ts">
+export type TNameProps = {
+	name: string
+	age: number
+}
+export type Person = {
+	id: string
+	age: number
+	name: string
+}
+type OmitStrict<T, K extends keyof T> = { [P in Exclude<keyof T, K>]: T[P] }
+
+type Pick<T, K extends keyof T> = {
+	[P in K]: T[P]
+}
+type Exclude<T, U> = T extends U ? never : T
+type OmitTyped<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
+</script>
+<script setup lang="ts">
+const props = defineProps<TNameProps>()
+</script>
+
+<template>
+	<p>{{ props.name }} {{ props.age }}</p>
+</template>
